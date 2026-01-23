ca47f4edd1532b99f6bc60b1bed456fb42276710
Kylian <kylian.gagnant@etu.univ-amu.fr>
2026-01-23 11:19:44 +0100

    Check project button fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 const Hero = () => {
     const scrollToProjects = () => {
-      document.getElementById('projets')?.scrollIntoView({ behavior: 'smooth' })
+      document.getElementById('projects')?.scrollIntoView({ behavior: 'smooth' })
     }
   
     return (
